9c9bb4fe07f8e3342bf65b9cb7de5ad97a8ae4bc
claudio-code <suporte2424@outlook.com>
2020-06-01 22:23:21 -0300

    alterado estilo das telas quando o usuario não está logado

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,60 +3,39 @@ import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(#0000, #424242);
+  background: linear-gradient(#2196f3, #000);
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 export const Content = styled.div`
-  width: 100%;
-  max-width: 315px;
+  width: 100vh;
+  max-width: 60vh;
   text-align: center;
+
   form {
     display: flex;
     flex-direction: column;
-    margin-top: 30px;
-    input {
-      background: rgba(0, 0, 0, 0.1);
-      border: 0;
-      border-radius: 4px;
-      height: 44px;
-      padding: 0 15px;
-      color: #FFF;
-      margin: 0 0 10px;
-      &::placeholder {
-        color: rgba(255, 255, 255, 0.7);
-      }
+    width: 60vh;
+    padding: 4vh;
+    border-radius: 5px;
+    background-color: #FFF;
+
+    h2 {
+      margin-bottom: 4vh;
+      color: #616161;
     }
 
-    span {
-      color: #fb6f91;
-      margin: 0 0 10px;
-      font-weight: bold;
-      align-self: center;
+    .MuiTextField-root {
+      margin: 3%;
     }
+
     button {
-      margin: 5px 0 0;
-      height: 44px;
-      background: #3b9eff;
-      font-weight: bold;
-      color: #FFF;
-      border: 0;
-      border-radius: 4px;
-      font-size: 16px;
-      transition: background 0.2%;
+      background-color: #0d47a1;
+
       &:hover {
-        background: ${darken(0.03, '#3b9eff')};
-      }
-    }
-    a {
-      color: #FFF;
-      margin-top: 15px;
-      font-size: 16px;
-      opacity: 0.8;
-      &:hover {
-        opacity: 1;
+        background-color: #2962ff;
       }
     }
   }
