9ef8947557bea2d06846b80c64feef6ecef1f227
claudio-code <suporte2424@outlook.com>
2020-06-01 22:20:57 -0300

    dominio para vaildar os dados do usuario

---
+++ src/domain/User.js
@@ -0,0 +1,25 @@
+import { toast } from 'react-toastify';
+
+export default class User {
+  constructor({ email, password }) {
+    this.email = this.validateEmail(email);
+    this.password = this.validatePassword(password);
+  }
+
+  validatePassword(password) {
+    const passwdLength = password.split('');
+    if (passwdLength.length < 4) {
+      toast.error('Senha com caracteres insufisientes.');
+    }
+
+    return password;
+  }
+
+  validateEmail(email) {
+    if (!/@/.test(email)) {
+      toast.error('Esse email é invalido.');
+    }
+
+    return email;
+  }
+}
