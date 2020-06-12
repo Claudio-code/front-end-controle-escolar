3afcb229e027e55953f46cf9fb2f0339181091df
claudio-code <suporte2424@outlook.com>
2020-06-12 19:24:21 -0300

    adicionado tela de dash do professor

---
+++ src/pages/Professores/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Professores() {
+  return (
+    <Container>
+      Professores Dash
+    </Container>
+  );
+}
+
+export default Professores;
