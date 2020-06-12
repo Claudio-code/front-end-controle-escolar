6a3bd194963233e2faa969fbe861a391c4baf350
claudio-code <suporte2424@outlook.com>
2020-06-12 19:25:00 -0300

    adicionada tela de dashboard das turmas

---
+++ src/pages/Turmas/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Turmas() {
+  return (
+    <Container>
+      Turmas dash
+    </Container>
+  );
+}
+
+export default Turmas;
