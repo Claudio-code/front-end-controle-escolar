481d022c8ee8f4a85c486c591175125b3b98dd5f
claudio-code <suporte2424@outlook.com>
2020-06-12 19:22:44 -0300

    adicionado tela do dash das disiplinas

---
+++ src/pages/Disiplinas/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Disiplinas() {
+  return (
+    <Container>
+      Disiplinas dash
+    </Container>
+  );
+}
+
+export default Disiplinas;
