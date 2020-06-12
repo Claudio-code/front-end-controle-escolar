f82b3daf1c3eecbfe36c4c133cd2ee43d682d03b
claudio-code <suporte2424@outlook.com>
2020-06-12 17:27:17 -0300

    criado componente da navbar da aplicacao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import SchoolIcon from '@material-ui/icons/School';
 
 import {
   Container,
   Title,
-  LogoutButton,
+  Button,
   ToolbarContainer,
+  ContainerLogo,
+  LogoutIcon,
+  ContainerMenus,
 } from './styles';
+import history from '../../services/history';
 
 export default function Header() {
+  const goStudent = () => history.push('/Alunos');
+  // const goStudent = () => history.push('/Alunos');
 
-  return(
+  return (
     <Container>
-       <AppBar position="static">
+      <AppBar position="static">
         <ToolbarContainer>
-          <Title variant="h6">
-            Sistema de controle escolar
-          </Title>
-          <LogoutButton color="inherit">
-            Logout
-          </LogoutButton>
+          <ContainerLogo>
+            <SchoolIcon />
+            <Title>
+              Sistema de controle escolar
+            </Title>
+          </ContainerLogo>
+          <ContainerMenus>
+            <Button>
+              Alunos
+            </Button>
+            <Button>
+              Professores
+            </Button>
+            <Button>
+              Disiplinas
+            </Button>
+            <Button>
+              Cursos
+            </Button>
+            <Button>
+              Turmas
+            </Button>
+          </ContainerMenus>
+          <Button>
+            <LogoutIcon />
+          </Button>
         </ToolbarContainer>
       </AppBar>
     </Container>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,19 +1,37 @@
 import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 export const Container = styled.div``;
 
+export const ContainerMenus = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`;
+
+export const ContainerLogo = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+
 export const ToolbarContainer = styled(Toolbar)`
+  display: flex;
   flex: 1;
   justify-content: space-between;
 `;
 
-export const Title = styled(Typography)`
+export const Title = styled(Typography).attrs({
+  variant: 'p',
+})``;
+
+export const Button = styled(button).attrs({
+  color: 'inherit',
+})`
   font-weight: bold;
 `;
 
-export const LogoutButton = styled(Button)`
-  font-weight: bold;
-`;
+export const LogoutIcon = styled(ExitToAppIcon)``;
