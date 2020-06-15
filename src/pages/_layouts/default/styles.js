3efb2bba59b434fd902789daca4a7b853dd87159
claudio-code <suporte2424@outlook.com>
2020-06-15 01:26:02 -0300

    corigido bug do css no background

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100vh;
+  position: absolute;
+  min-width: 100%;
+  min-height: 100%;
   background: linear-gradient(#ffc107, #ffca28, #ffd54f);
 `;
