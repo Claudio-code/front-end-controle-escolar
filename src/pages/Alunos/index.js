35c944e21d749a2beb2a603b6835fbe3f41d1796
claudio-code <suporte2424@outlook.com>
2020-06-15 01:25:10 -0300

    adicionado operacoes possiveis com o aluno

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import ContextTitle from '../../components/ContextTitle';
+import CardMenu from '../../components/CardMenu';
 import {
   Container,
   Title,
+  ContainerCard,
 } from './styles';
 
 function Alunos() {
@@ -13,6 +15,64 @@ function Alunos() {
         Opções do Aluno
       </Title>
       <ContextTitle title="Cadastro" />
+      <ContainerCard>
+        <CardMenu
+          title="Cadastro completo"
+          content="Cadastro do aluno, endereço e o responsavel."
+        />
+        <CardMenu
+          title="Cadastro do Aluno"
+          content="Cadastro do aluno, deixando pendente o endereço e o responsavel."
+        />
+        <CardMenu
+          title="Cadastro do Aluno e endereço"
+          content="Cadastro do aluno e o endereço deixando pendente o responsavel."
+        />
+        <CardMenu
+          title="Cadastro do Aluno e o seu responsavel"
+          content="Cadastro do aluno e o responsavel deixando pendente o endereço."
+        />
+      </ContainerCard>
+
+      <ContextTitle title="Editar" />
+      <ContainerCard>
+        <CardMenu
+          title="Editar todos os registros"
+          content="Editar todos os dados do aluno, endereço e o responsavel."
+        />
+        <CardMenu
+          title="Editar do Aluno"
+          content="Editar todos os dados aluno, deixando imutavel o endereço e o responsavel."
+        />
+        <CardMenu
+          title="Editar do Aluno e endereço"
+          content="Editar todos os dados aluno e o endereço deixando imutavel o responsavel."
+        />
+        <CardMenu
+          title="Editar do Aluno e o seu responsavel"
+          content="Editar todos os dados aluno e o responsavel deixando imutavel o endereço."
+        />
+      </ContainerCard>
+
+      <ContextTitle title="Outros" />
+      <ContainerCard>
+        <CardMenu
+          title="Matricular"
+          content="Fazer a matricula do aluno."
+        />
+        <CardMenu
+          title="Cadastro o Aluno em uma turma"
+          content="Cadastrar o aluno em uma turma."
+        />
+        <CardMenu
+          title="Trocar o aluno de turma"
+          content="Mover o aluno para uma turma que tenha vagas."
+        />
+        <CardMenu
+          title="Cancelar a matricula"
+          content="Desativar a matricula do aluno."
+        />
+      </ContainerCard>
     </Container>
   );
 }
