34ad5a6b3491d05cfb6b98373c99f8b61cac7fa6
claudio-code <suporte2424@outlook.com>
2020-06-17 13:46:41 -0300

    adicionado rota do aluno

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ import Route from './route';
 import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import Alunos from '../pages/Alunos';
+import AlunoFormularioCompleto from '../pages/AlunoFormularioCompleto';
+
 import Professores from '../pages/Professores';
 import Disiplinas from '../pages/Disiplinas';
 import Cursos from '../pages/Cursos';
@@ -15,7 +17,8 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/Alunos" component={Alunos} isPrivate />
+      <Route path="/Alunos" exact component={Alunos} isPrivate />
+      <Route path="/Alunos/FormularioCompleto" exact component={AlunoFormularioCompleto} isPrivate />
       <Route path="/Professores" component={Professores} isPrivate />
       <Route path="/Disiplinas" component={Disiplinas} isPrivate />
       <Route path="/Cursos" component={Cursos} isPrivate />
