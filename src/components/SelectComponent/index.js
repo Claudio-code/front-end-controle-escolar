927fef2d945a4f7dd6969e5885bd415c1b8fee50
claudio-code <suporte2424@outlook.com>
2020-06-18 19:17:28 -0300

    criado componente generico para o campo select dos formularios

---
+++ src/components/SelectComponent/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
+
+function SelectComponent(props) {
+  const {
+    id, label, value, onchangeValue, options,
+  } = props;
+
+  return (
+    <div>
+      <FormControl fullWidth>
+        <InputLabel id={id}>{ label }</InputLabel>
+        <Select
+          labelId={id}
+          id={id}
+          value={value}
+          onChange={onchangeValue}
+        >
+          {options.map((item) => (
+            <MenuItem value={item}>{ item }</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+SelectComponent.propTypes = {
+  id: PropTypes.string,
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onchangeValue: PropTypes.func,
+  options: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string])),
+};
+
+SelectComponent.defaultProps = {
+  id: '1',
+  label: '',
+  value: '',
+  onchangeValue: () => {},
+  options: [],
+};
+
+export default SelectComponent;
