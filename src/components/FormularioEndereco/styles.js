7c6e19a972304ce2cf1f243dc3f83661c7ac6e46
claudio-code <suporte2424@outlook.com>
2020-06-20 23:07:12 -0300

    iniciando os estilos do forumlario

---
+++ src/components/FormularioEndereco/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+export const Container = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+`;
