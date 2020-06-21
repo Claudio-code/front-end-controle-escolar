03ab1c4124d05919bc23950f38df6e728f9c4f8a
claudio-code <suporte2424@outlook.com>
2020-06-20 23:10:32 -0300

    adicionado estilo inicial do componente

---
+++ src/components/FormularioResponsavel/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+export const Container = styled.div`
+  margin-top: 20vh;
+`;
+
+export const Title = styled(Typography).attrs({
+  variant: 'h5',
+})``;
