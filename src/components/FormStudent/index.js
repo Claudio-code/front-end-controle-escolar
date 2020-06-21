696ee8d02272304969e30939014eb987aca24961
claudio-code <suporte2424@outlook.com>
2020-06-21 04:37:53 -0300

    abstraido status dos inputs do componente

--- src/components/FormStudent/index.js
+++ src/components/FormStudent/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from '@material-ui/core';
 
@@ -11,31 +11,36 @@ import {
   ContainerHeader,
 } from './styles';
 
-function FormStudent({
-  title,
-  name,
-  setName,
-  email,
-  setEmail,
-  cnh,
-  setCnh,
-  cpf,
-  setCpf,
-  rg,
-  setRg,
-  age,
-  setAge,
-  ethnicity,
-  setEthnicity,
-  sex,
-  setSex,
-}) {
-  const [nameStatus, setNameStatus] = useState(false);
-  const [emailStatus, setEmailStatus] = useState(false);
-  const [cnhStatus,] = useState(false);
-  const [cpfStatus, setCpfStatus] = useState(false);
-  const [rgStatus, setRgStatus] = useState(false);
-  const [ageStatus, setAgeStatus] = useState(false);
+function FormStudent(props) {
+  const {
+    title,
+    name,
+    setName,
+    nameStatus,
+    setNameStatus,
+    email,
+    setEmail,
+    emailStatus,
+    setEmailStatus,
+    cnh,
+    setCnh,
+    cpf,
+    setCpf,
+    cpfStatus,
+    setCpfStatus,
+    rg,
+    setRg,
+    rgStatus,
+    setRgStatus,
+    age,
+    setAge,
+    ageStatus,
+    setAgeStatus,
+    ethnicity,
+    setEthnicity,
+    sex,
+    setSex,
+  } = props;
 
   const handleChangeName = (event) => {
     const result = Student.validateName(event.target.value);
@@ -76,110 +81,109 @@ function FormStudent({
       <ContainerHeader>
         <Title>{ title }</Title>
       </ContainerHeader>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <InputComponent
-              id="idName"
-              label="Nome do Estudante"
-              value={name}
-              type="text"
-              onchangeValue={handleChangeName}
-              errorStatus={nameStatus}
-              errorMessage="Insira um valor de nome sem numeros ou simbolos."
-              infoMessage="Insira o nome do estudante."
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <InputComponent
-              id="idEmail"
-              label="E-mail do Estudante"
-              value={email}
-              type="email"
-              onchangeValue={handleChangeEmail}
-              errorStatus={emailStatus}
-              errorMessage="Insira um email valido."
-              infoMessage="Insira um email de contato para o estudante."
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <InputComponent
-              id="idCpf"
-              label="Cpf do Estudante"
-              value={cpf}
-              type="number"
-              onchangeValue={handleChangeCpf}
-              errorStatus={cpfStatus}
-              errorMessage="Erro insira um cpf valido, apenas os numeros."
-              infoMessage="Insira um cpf valido, apenas os numeros."
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <InputComponent
-              id="idCnh"
-              label="Cnh do Estudante"
-              value={cnh}
-              type="number"
-              onchangeValue={handleChangeCnh}
-              errorStatus={cnhStatus}
-              errorMessage="Erro insira um cnh valido, apenas os numeros."
-              infoMessage="Insira um cnh valido, apenas os numeros."
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <InputComponent
-              id="idAge"
-              label="Idade do Estudante"
-              value={age}
-              type="number"
-              onchangeValue={handleChangeAge}
-              errorStatus={ageStatus}
-              errorMessage="Erro insira um idade valido, apenas numeros."
-              infoMessage="Insira um idade valido, apenas numeros."
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <SelectComponent
-              id="idSex"
-              value={sex}
-              onchangeValue={handleChangeSex}
-              label="Orientação sexual do Estudante"
-              options={[
-                'Masculino',
-                'Feminino',
-                'Outro',
-              ]}
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <InputComponent
-              id="idRg"
-              label="Rg do Estudante"
-              value={rg}
-              type="number"
-              onchangeValue={handleChangeRg}
-              errorStatus={rgStatus}
-              errorMessage="Erro insira um rg valido, apenas numeros."
-              infoMessage="Insira um rg valido, apenas numeros."
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <SelectComponent
-              id="idSex"
-              value={ethnicity}
-              onchangeValue={handleChangeEthnicity}
-              label="Etnia do estudante"
-              options={[
-                'Brancos',
-                'Negros',
-                'Indígenas',
-                'Pardos',
-                'Mulatos',
-                'Caboclos',
-                'Cafuzos',
-              ]}
-            />
-          </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <InputComponent
+            id="idName"
+            label="Nome do Estudante"
+            value={name}
+            type="text"
+            onchangeValue={handleChangeName}
+            errorStatus={nameStatus}
+            errorMessage="Insira um valor de nome sem numeros ou simbolos."
+            infoMessage="Insira o nome do estudante."
+          />
         </Grid>
+        <Grid item xs={6}>
+          <InputComponent
+            id="idEmail"
+            label="E-mail do Estudante"
+            value={email}
+            type="email"
+            onchangeValue={handleChangeEmail}
+            errorStatus={emailStatus}
+            errorMessage="Insira um email valido."
+            infoMessage="Insira um email de contato para o estudante."
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <InputComponent
+            id="idCpf"
+            label="Cpf do Estudante"
+            value={cpf}
+            type="number"
+            onchangeValue={handleChangeCpf}
+            errorStatus={cpfStatus}
+            errorMessage="Erro insira um cpf valido, apenas os numeros."
+            infoMessage="Insira um cpf valido, apenas os numeros."
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <InputComponent
+            id="idCnh"
+            label="Cnh do Estudante"
+            value={cnh}
+            type="number"
+            onchangeValue={handleChangeCnh}
+            errorMessage="Erro insira um cnh valido, apenas os numeros."
+            infoMessage="Insira um cnh valido, apenas os numeros."
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <InputComponent
+            id="idAge"
+            label="Idade do Estudante"
+            value={age}
+            type="number"
+            onchangeValue={handleChangeAge}
+            errorStatus={ageStatus}
+            errorMessage="Erro insira um idade valido, apenas numeros."
+            infoMessage="Insira um idade valido, apenas numeros."
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <SelectComponent
+            id="idSex"
+            value={sex}
+            onchangeValue={handleChangeSex}
+            label="Orientação sexual do Estudante"
+            options={[
+              'Masculino',
+              'Feminino',
+              'Outro',
+            ]}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <InputComponent
+            id="idRg"
+            label="Rg do Estudante"
+            value={rg}
+            type="number"
+            onchangeValue={handleChangeRg}
+            errorStatus={rgStatus}
+            errorMessage="Erro insira um rg valido, apenas numeros."
+            infoMessage="Insira um rg valido, apenas numeros."
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <SelectComponent
+            id="idSex"
+            value={ethnicity}
+            onchangeValue={handleChangeEthnicity}
+            label="Etnia do estudante"
+            options={[
+              'Brancos',
+              'Negros',
+              'Indígenas',
+              'Pardos',
+              'Mulatos',
+              'Caboclos',
+              'Cafuzos',
+            ]}
+          />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
@@ -187,17 +191,27 @@ function FormStudent({
 FormStudent.propTypes = {
   title: PropTypes.string,
   name: PropTypes.string,
+  nameStatus: PropTypes.bool,
+  setNameStatus: PropTypes.func,
   setName: PropTypes.func,
   email: PropTypes.string,
   setEmail: PropTypes.func,
+  emailStatus: PropTypes.bool,
+  setEmailStatus: PropTypes.func,
   cnh: PropTypes.string,
   setCnh: PropTypes.func,
   cpf: PropTypes.string,
   setCpf: PropTypes.func,
+  cpfStatus: PropTypes.bool,
+  setCpfStatus: PropTypes.func,
   rg: PropTypes.string,
   setRg: PropTypes.func,
+  rgStatus: PropTypes.bool,
+  setRgStatus: PropTypes.func,
   age: PropTypes.string,
   setAge: PropTypes.func,
+  ageStatus: PropTypes.bool,
+  setAgeStatus: PropTypes.func,
   ethnicity: PropTypes.string,
   setEthnicity: PropTypes.func,
   sex: PropTypes.string,
@@ -208,16 +222,26 @@ FormStudent.defaultProps = {
   title: 'Titulo do form',
   name: '',
   setName: () => {},
+  nameStatus: false,
+  setNameStatus: () => {},
   email: '',
   setEmail: () => {},
+  emailStatus: false,
+  setEmailStatus: () => {},
   cnh: '',
   setCnh: () => {},
   cpf: '',
   setCpf: () => {},
+  cpfStatus: false,
+  setCpfStatus: () => {},
   rg: '',
   setRg: () => {},
+  rgStatus: false,
+  setRgStatus: () => {},
   age: '',
   setAge: () => {},
+  ageStatus: false,
+  setAgeStatus: () => {},
   ethnicity: '',
   setEthnicity: () => {},
   sex: '',
