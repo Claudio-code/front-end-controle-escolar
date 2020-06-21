a2afda2eb1cc0366f18e959f2543683aa243765f
claudio-code <suporte2424@outlook.com>
2020-06-21 04:37:08 -0300

    abstraido status do input no formulario do responsavel

--- src/components/FormResponsible/index.js
+++ src/components/FormResponsible/index.js
@@ -5,20 +5,55 @@ import { Grid, Paper } from '@material-ui/core';
 import { Container } from './styles';
 import InputComponent from '../InputComponent';
 import SelectComponent from '../SelectComponent';
+import Responsible from "../../domain/Responsible";
 
 function FormResponsible(props) {
   const {
     name,
     setName,
+    nameStatus,
+    setNameStatus,
     email,
     setEmail,
+    emailStatus,
+    setEmailStatus,
     kinship,
     setKinship,
     cpf,
     setCpf,
+    cpfStatus,
+    setCpfStatus,
     rg,
     setRg,
+    rgStatus,
+    setRgStatus,
   } = props;
+
+  const handleChangeKinship = (event) => setKinship(event.target.value);
+  const handleChangeName = (event) => {
+    const result = Responsible.validateName(event.target.value);
+    setNameStatus(result);
+    setName(event.target.value);
+  };
+
+  const handleChangeEmail = (event) => {
+    const result = Responsible.validateEmail(event.target.value);
+    setEmailStatus(result);
+    setEmail(event.target.value);
+  };
+
+  const handleChangeCpf = (event) => {
+    const result = Responsible.validateCpf(event.target.value);
+    setCpfStatus(result);
+    setCpf(event.target.value);
+  };
+
+  const handleChangeRg = (event) => {
+    const result = Responsible.validateRg(event.target.value);
+    setRgStatus(result);
+    setRg(event.target.value);
+  };
+
   return (
     <Container>
       <Grid container spacing={3}>
@@ -27,28 +62,29 @@ function FormResponsible(props) {
             id="idName"
             label="Nome do Responsavel"
             value={name}
-            type="number"
-              // onchangeValue={handleChangeCnh}
+            errorStatus={nameStatus}
+            onchangeValue={handleChangeName}
             errorMessage="Erro insira um nome, apenas letras."
             infoMessage="Insira apenas letras."
           />
         </Grid>
         <Grid item xs={6}>
           <InputComponent
-            id="idCpf"
-            label="Cpf do Responsavel"
-            value={cpf}
-            type="number"
-              // onchangeValue={handleChangeCnh}
-            errorMessage="Erro insira um cpf valido, apenas os numeros."
-            infoMessage="Insira um cpf valido, apenas os numeros."
+            id="idEmail"
+            label="Email do Responsavel"
+            value={email}
+            errorStatus={emailStatus}
+            type="email"
+            onchangeValue={handleChangeEmail}
+            errorMessage="Erro insira um Email valido."
+            infoMessage="Insira um Email valido."
           />
         </Grid>
         <Grid item xs={4}>
           <SelectComponent
             id="idKinship"
             value={kinship}
-            // onchangeValue={handleChangeSex}
+            onchangeValue={handleChangeKinship}
             label="Tipo do parentesco"
             options={[
               'Pai',
@@ -62,8 +98,9 @@ function FormResponsible(props) {
             id="idCpf"
             label="Cpf do Responsavel"
             value={cpf}
+            errorStatus={cpfStatus}
             type="number"
-              // onchangeValue={handleChangeCnh}
+            onchangeValue={handleChangeCpf}
             errorMessage="Erro insira um cpf valido, apenas os numeros."
             infoMessage="Insira um cpf valido, apenas os numeros."
           />
@@ -73,8 +110,9 @@ function FormResponsible(props) {
             id="idRg"
             label="Rg do Responsavel"
             value={rg}
+            errorStatus={rgStatus}
             type="number"
-              // onchangeValue={handleChangeCnh}
+            onchangeValue={handleChangeRg}
             errorMessage="Erro insira um rg valido, apenas os numeros."
             infoMessage="Insira um rg valido, apenas os numeros."
           />
@@ -87,27 +125,43 @@ function FormResponsible(props) {
 FormResponsible.propTypes = {
   name: PropTypes.string,
   setName: PropTypes.func,
+  nameStatus: PropTypes.bool,
+  setNameStatus: PropTypes.func,
   email: PropTypes.string,
   setEmail: PropTypes.func,
+  emailStatus: PropTypes.bool,
+  setEmailStatus: PropTypes.func,
   kinship: PropTypes.string,
   setKinship: PropTypes.func,
   cpf: PropTypes.string,
   setCpf: PropTypes.func,
+  cpfStatus: PropTypes.bool,
+  setCpfStatus: PropTypes.func,
   rg: PropTypes.string,
   setRg: PropTypes.func,
+  rgStatus: PropTypes.bool,
+  setRgStatus: PropTypes.func,
 };
 
 FormResponsible.defaultProps = {
   name: '',
   setName: () => {},
+  nameStatus: false,
+  setNameStatus: () => {},
   email: '',
   setEmail: () => {},
+  emailStatus: false,
+  setEmailStatus: () => {},
   kinship: '',
   setKinship: () => {},
   cpf: '',
   setCpf: () => {},
+  cpfStatus: false,
+  setCpfStatus: () => {},
   rg: '',
   setRg: () => {},
+  rgStatus: false,
+  setRgStatus: () => {},
 };
 
 export default FormResponsible;
