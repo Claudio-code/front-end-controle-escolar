2391284196ded1ce0a6df28b1f9a39a99b576241
claudio-code <suporte2424@outlook.com>
2020-06-20 23:06:54 -0300

    adicionado os parametros necessarios para o formulario de endereco

--- src/components/FormularioEndereco/index.js
+++ src/components/FormularioEndereco/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid } from '@material-ui/core';
 
-function FormularioEndereço() {
+import { Container } from './styles';
+import InputComponent from '../InputComponent';
+
+function FormularioEndereço(props) {
+  const {
+    cep,
+    setCep,
+    city,
+    setCity,
+    country,
+    setCountry,
+    publicPlace,
+    setPublicPlace,
+    number,
+    setNumber,
+  } = props;
+
   return (
-    <div>Form Endereco</div>
+    <Container>
+      dqdwq qwdqw
+    </Container>
   );
 }
+
+FormularioEndereço.propTypes = {
+  cep: PropTypes.string,
+  setCep: PropTypes.func,
+  city: PropTypes.string,
+  setCity: PropTypes.func,
+  country: PropTypes.string,
+  setCountry: PropTypes.func,
+  publicPlace: PropTypes.string,
+  setPublicPlace: PropTypes.func,
+  number: PropTypes.string,
+  setNumber: PropTypes.func,
+};
+
+FormularioEndereço.defaultProps = {
+  cep: '',
+  setCep: () => {},
+  city: '',
+  setCity: () => {},
+  country: '',
+  setCountry: () => {},
+  publicPlace: '',
+  setPublicPlace: () => {},
+  number: '',
+  setNumber: () => {},
+};
 
 export default FormularioEndereço;
