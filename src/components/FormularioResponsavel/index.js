fba4b00049119a2b0cd25eac1f7769fdb7f91c34
claudio-code <suporte2424@outlook.com>
2020-06-20 23:07:36 -0300

    adicionado os valores e proptypes do componente

--- src/components/FormularioResponsavel/index.js
+++ src/components/FormularioResponsavel/index.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Paper } from '@material-ui/core';
 
-function FormularioResponsavel() {
+import { Container } from './styles';
+import InputComponent from '../InputComponent';
+
+function FormularioResponsavel(props) {
+  const {
+    name,
+    setName,
+    email,
+    setEmail,
+    kinship,
+    setKinship,
+    cpf,
+    setCpf,
+    rg,
+    setRg,
+  } = props;
   return (
-    <div>FormularioResponsavel</div>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item xs={6}>
+          <Paper>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
+
+FormularioResponsavel.propTypes = {
+  name: PropTypes.string,
+  setName: PropTypes.func,
+  email: PropTypes.string,
+  setEmail: PropTypes.func,
+  kinship: PropTypes.string,
+  setKinship: PropTypes.func,
+  cpf: PropTypes.string,
+  setCpf: PropTypes.func,
+  rg: PropTypes.string,
+  setRg: PropTypes.func,
+};
+
+FormularioResponsavel.defaultProps = {
+  name: '',
+  setName: () => {},
+  email: '',
+  setEmail: () => {},
+  kinship: '',
+  setKinship: () => {},
+  cpf: '',
+  setCpf: () => {},
+  rg: '',
+  setRg: () => {},
+};
 
 export default FormularioResponsavel;
