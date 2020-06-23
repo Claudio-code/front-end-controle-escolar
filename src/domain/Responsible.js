d1bcb63d4dc7393430334c40472527e58819fd23
claudio-code <suporte2424@outlook.com>
2020-06-23 16:32:40 -0300

    adicionado contrutor para criar objeto e metodo para validar o input

--- src/domain/Responsible.js
+++ src/domain/Responsible.js
@@ -1,3 +1,16 @@
 import GenericPeople from './GenericPeople';
 
-export default class Responsible extends GenericPeople {}
+export default class Responsible extends GenericPeople {
+  constructor(name, email, rg, cpf, kinship) {
+    super();
+    this.name = name;
+    this.email = email;
+    this.rg = rg;
+    this.cpf = cpf;
+    this.kinship = kinship;
+  }
+
+  static validateAllData(nameStatus, emailStatus, rgStatus, cpfStatus) {
+    return !(nameStatus && emailStatus && rgStatus && cpfStatus);
+  }
+}
--- src/domain/Student.js
+++ src/domain/Student.js
@@ -1,3 +1,27 @@
-import GenericPeople from "./GenericPeople";
+import GenericPeople from './GenericPeople';
 
-export default class Student extends GenericPeople {}
+export default class Student extends GenericPeople {
+  constructor(name, email, rg, cpf, cnh, age, ethnicity, sex) {
+    super();
+    this.name = name;
+    this.email = email;
+    this.rg = rg;
+    this.cpf = cpf;
+    this.cnh = cnh;
+    this.age = age;
+    this.ethnicity = ethnicity;
+    this.sex = sex;
+  }
+
+  setAddress(address) {
+    this.Address = address;
+  }
+
+  setResponsible(responsible) {
+    this.Responsible = responsible;
+  }
+
+  static validateAllData(nameStatus, emailStatus, ageStatus, rgStatus, cpfStatus) {
+    return !(nameStatus && emailStatus && ageStatus && rgStatus && cpfStatus);
+  }
+}
