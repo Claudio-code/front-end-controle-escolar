e434f4ab57558c6eac42006c8d9bb66a15e69012
claudio-code <suporte2424@outlook.com>
2020-06-23 16:31:55 -0300

    adicionado contrutor para criar objeto do endereco

--- src/domain/Andress.js
+++ src/domain/Andress.js
@@ -1,6 +1,19 @@
+/* eslint-disable camelcase */
 import cepPromise from 'cep-promise';
 
 export default class Andress {
+  constructor(city, cep, country, public_place, number) {
+    this.city = city;
+    this.cep = cep;
+    this.country = country;
+    this.public_place = public_place;
+    this.number = number;
+  }
+
+  static validateAllData(cityStatus, cepStatus, countryStatus, publicPlaceStatus, numberStatus) {
+    return !(cityStatus && cepStatus && countryStatus && publicPlaceStatus && numberStatus);
+  }
+
   static async validarCep(cep) {
     try {
       const response = await cepPromise(cep);
