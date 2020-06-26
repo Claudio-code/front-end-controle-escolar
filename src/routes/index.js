f91f675c34ea604bed324b9a0fd5904af5616ea6
claudio-code <suporte2424@outlook.com>
2020-06-25 23:09:25 -0300

    adicionada rota de cadastro de aluno com endereco e apenas aluno

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import SignIn from '../pages/SignIn';
 import Dashboard from '../pages/Dashboard';
 import Alunos from '../pages/Alunos';
 import StudentCompleteForm from '../pages/StudentCompleteForm';
+import StudentAndAddressRegistration from '../pages/StudentAndAddressRegistration';
+import StudentRegistration from '../pages/StudentRegistration';
 
 import Professores from '../pages/Professores';
 import Disiplinas from '../pages/Disiplinas';
@@ -18,7 +20,24 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/Alunos" exact component={Alunos} isPrivate />
-      <Route path="/Alunos/FormularioCompleto" exact component={StudentCompleteForm} isPrivate />
+      <Route
+        path="/Alunos/FormularioCompleto"
+        component={StudentCompleteForm}
+        exact
+        isPrivate
+      />
+      <Route
+        path="/Alunos/FormularioCadastro"
+        component={StudentRegistration}
+        exact
+        isPrivate
+      />
+      <Route
+        path="/Alunos/StudentAndAddressRegistration"
+        component={StudentAndAddressRegistration}
+        exact
+        isPrivate
+      />
       <Route path="/Professores" component={Professores} isPrivate />
       <Route path="/Disiplinas" component={Disiplinas} isPrivate />
       <Route path="/Cursos" component={Cursos} isPrivate />
