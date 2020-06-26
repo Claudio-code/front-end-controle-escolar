669c5aca83d60c29661f26e4ccf63d2bbe68fd16
claudio-code <suporte2424@outlook.com>
2020-06-25 23:10:43 -0300

    adicionado action do cadastro de estudante e dele com o endereco

--- src/store/modules/student/actions.js
+++ src/store/modules/student/actions.js
@@ -1,20 +1,27 @@
-export function completeStudentRegistration(Student) {
+export function completeStudentRegistrationAction(Student) {
   return {
     type: '@student/COMPLETE_STUDENT_REGISTRATION',
     payload: { Student },
   };
 }
 
-export function StudentRegistrationAndAnddress(Student) {
+export function StudentRegistrationAndAnddressAction(Student) {
   return {
     type: '@student/STUDENT_REGISTRATION_AND_ANDDRESS',
     payload: { Student },
   };
 }
 
-export function StudentRegistrationAndResponsible(Student) {
+export function StudentRegistrationAndResponsibleAction(Student) {
   return {
     type: '@student/STUDENT_REGISTRATION_AND_RESPONSIBLE',
+    payload: { Student },
+  };
+}
+
+export function StudentRegistrationAction(Student) {
+  return {
+    type: '@student/STUDENT_REGISTRATION',
     payload: { Student },
   };
 }
