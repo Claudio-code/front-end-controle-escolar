bb769b5e9becd968970d8a1743e3fd134a57e073
claudio-code <claudio@digitalone.com.br>
2020-07-05 18:27:31 -0300

    alterado estilo do codigo foi alterado para manter um padrão entre todos os componentes

--- src/components/InputComponent/styles.js
+++ src/components/InputComponent/styles.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 import { InputLabel } from '@material-ui/core';
 
-export const Label = styled(InputLabel).attrs({
-  autoComplete: false,
-})`
+export const Label = styled(InputLabel)`
   font-size: 2.2vh !important;
 `;
