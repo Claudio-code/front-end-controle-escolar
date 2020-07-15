f1d964b4cf1e2753a9567f19a72c5dd8373ed310
claudio-code <suporte2424@outlook.com>
2020-07-14 21:25:27 -0300

    adicionado componente q atualiza e cria um responsavel pelo aluno

---
+++ src/components/ModalUpdateResponsible/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
+export const Title = styled(Typography).attrs({
+  variant: 'h5',
+})`
+  font-weight: bold !important;
+  color: #616161 !important;
+`;
